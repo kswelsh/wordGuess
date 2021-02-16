963cefdad59cd7c0cd8b8d0f1c69a6acc081ce11
Kyle Welsh <kswelsh@noctrl.edu>
2021-02-15 19:04:03 -0600

    modularization (real final)

--- src/wordGuessAssignment.cpp
+++ src/wordGuessAssignment.cpp
@@ -15,8 +15,65 @@ using namespace std;
 #include "Player.h"
 #include "GameState.h"
 
+void postGuessDisplay(GameState &gInstance)
+// pre: parm must be the game in which user wants to display
+// post: outputs current attempts left, incorrect letters, and incorrect words
+{
+	cout << endl << "You have " << gInstance.getAttemptsLeft() << " attempt(s) left!" << endl;
+	cout << "Current incorrect letters: ";
+	for (int i = 0; i < gInstance.getIncorrectLettersSize(); i++)
+	{
+		cout << gInstance.getIncorrectLetter(i) << " ";
+	}
+	cout << endl;
+	cout << "Current incorrect words: ";
+	for (int i = 0; i < gInstance.getIncorrectWordsSize(); i++)
+	{
+		cout << gInstance.getIncorrectWord(i) << " ";
+	}
+	cout << endl;
+}
+void playerStatusUpdate(Player &pData, GameState &gInstance, bool &playerWin)
+// pre: must pass in player data user wants to update, current game instance, and value of if the player has won or not
+// post: displays if player won or lost, increments players total wins or losses, and resets playerWin variable
+{
+	if(playerWin == true)
+	{
+		cout << endl << "Nice! You Win! The correct word was " << gInstance.getCurrentWord() << "." << endl;
+		pData.incPlayerWins();
+		playerWin = false;
+	}
+	else
+	{
+		cout << endl << "Sorry... You Lose! The correct word is " << gInstance.getCurrentWord() << "." << endl;
+		pData.incPlayerLosses();
+	}
+}
+void populate(Dictionary &dData)
+// pre: parm must be the dictionary you want to populate
+// post: dictionary is populated from either a chosen file or chosen user words
+{
+	string fileChoice;
+	int userPopChoice;
+
+	cout << "=========================" << endl << "|    DICTIONARY MENU    |" << endl << "=========================" << endl;
+	cout << "| 1) Through File       |" << endl << "| 2) Through User Input |" << endl << "=========================" << endl;
+	cout << "Type a number listed to choose how to populate the dictionary:" << endl;
+	cin >> userPopChoice;
+	if (userPopChoice == 1)
+	{
+		cout << "Enter the name of the file you are using: " << endl;
+		cin >> fileChoice;
+		dData.populateWithFile(fileChoice);
+	}
+	else
+	{
+		dData.populateWithConsole();
+	}
+}
+
 void spaceDelimit(string word)
-// pre: for this function to perform correctly; passed word must be at least 2 characters
+// pre: for this function to perform correctly; parm word must be at least 2 characters
 // post: given word is printed out as space delimited
 {
 	for (int i = 0; i < word.size(); i++)
@@ -30,27 +87,13 @@ int main()
 	Dictionary dictionaryData;
 	Player playerData;
 	GameState gameInstance;
-	string userPlayChoice;
-	string fileChoice;
+
 	string randomWord;
 	string userGuess;
+	string userPlayChoice;
 	bool playerWin;
-	int userPopChoice;
 
-	cout << "=========================" << endl << "|    DICTIONARY MENU    |" << endl << "=========================" << endl;
-	cout << "| 1) Through File       |" << endl << "| 2) Through User Input |" << endl << "=========================" << endl;
-	cout << "Type a number listed to choose how to populate the dictionary:" << endl;
-	cin >> userPopChoice;
-	if (userPopChoice == 1)
-	{
-		cout << "Enter the name of the file you are using: " << endl;
-		cin >> fileChoice;
-		dictionaryData.populateWithFile(fileChoice);
-	}
-	else
-	{
-		dictionaryData.populateWithConsole();
-	}
+	populate(dictionaryData);
 
 	cout << "Would you like to play the word guess game? (Type 'y' for yes OR 'n' for no): " << endl;
 	cin >> userPlayChoice;
@@ -60,7 +103,6 @@ int main()
 		gameInstance.setCurrentWord(randomWord);
 		gameInstance.setAttemptsLeft(randomWord.size());
 		gameInstance.hideStateOfWord();
-
 		while (gameInstance.getAttemptsLeft() != 0 && playerWin == false)
 		{
 			cout << "Current Word: ";
@@ -73,36 +115,13 @@ int main()
 
 			if (playerWin == false && gameInstance.getAttemptsLeft() > 0)
 			{
-				cout << endl << "You have " << gameInstance.getAttemptsLeft() << " attempt(s) left!" << endl;
-				cout << "Current incorrect letters: ";
-				for (int i = 0; i < gameInstance.getIncorrectLettersSize(); i++)
-					{
-						cout << gameInstance.getIncorrectLetter(i) << " ";
-					}
-				cout << endl;
-				cout << "Current incorrect words: ";
-				for (int i = 0; i < gameInstance.getIncorrectWordsSize(); i++)
-					{
-						cout << gameInstance.getIncorrectWord(i) << " ";
-					}
-				cout << endl;
+				postGuessDisplay(gameInstance);
 			}
 		}
-
 		gameInstance.clearIncorrectLetters();
 		gameInstance.clearIncorrectWords();
 
-		if(playerWin == true)
-		{
-			cout << endl << "Nice! You Win! The correct word was " << gameInstance.getCurrentWord() << "." << endl;
-			playerData.incPlayerWins();
-			playerWin = false;
-		}
-		else
-		{
-			cout << endl << "Sorry... You Lose! The correct word is " << gameInstance.getCurrentWord() << "." << endl;
-			playerData.incPlayerLosses();
-		}
+		playerStatusUpdate(playerData, gameInstance, playerWin);
 
 		cout << "Would you like to play the word guess game? (Type 'y' for yes OR 'n' for no): " << endl;
 		cin >> userPlayChoice;
